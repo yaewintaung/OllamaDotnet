4d1873141d949e6bed407bb69ffab567ed84cf12
yaewintaung <yaewintaung.thedev@gmail.com>
2025-12-10 16:43:05 +0630

    gitignore added

--- obj/Debug/net10.0/LocalLLMAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/LocalLLMAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalLLMAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7872aa2d44253fd08776d4a3f0d53e7058aceb54")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalLLMAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalLLMAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
